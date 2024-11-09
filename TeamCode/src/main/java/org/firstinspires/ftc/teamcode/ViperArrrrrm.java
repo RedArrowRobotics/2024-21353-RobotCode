f164f5be8085dce09b470c276664b46094b91820
Henyahyah <henry.schrauben@gmail.com>
2024-11-09 13:16:20 -0500

    Viper Stages Work
    you can press the buttons and it will automatically go to low or high buckets or home (0, lowered). deb doesn't work.
    
    i also started making the autonomous match the teleop but didnt do really anything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperArrrrrm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperArrrrrm.java
@@ -23,42 +23,46 @@ public class ViperArrrrrm {
         viperArm.setDirection(DcMotor.Direction.REVERSE);
         viperArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         viperArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        viperArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        viperArm.setTargetPosition(0);
+        viperArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         touchSensor = hwm.get(TouchSensor.class, Constants.TOUCH_SENSOR);
     }
 
     void home(){
+        viperArm.setPower(1);
         viperArm.setTargetPosition(0);
     }
     void highBucket(){
-        viperArm.setTargetPosition(5914);
+        viperArm.setPower(1);
+        viperArm.setTargetPosition(11300);
     }
     void lowBucket(){
-        viperArm.setTargetPosition(2500);
+        viperArm.setPower(1);
+        viperArm.setTargetPosition(7000);
     }
 
     void operateArm(Telemetry telemetry, double armPower){
-        double max = 1;
-        max = Math.max(max, Math.abs(armPower));
-        if (armPower > max) {
-            armPower = max;
-        }
-
-        if (touchSensor.isPressed() && armPower < 0) {
-            armPower = 0;
-            viperArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            telemetry.addData("Deb", "Pressed");
-        } else {
-            telemetry.addData("Deb", "Not Pressed");
-        }
-        //upper limit
-        if (viperArm.getCurrentPosition() >= 5914 && armPower > 0){ //this is viper max height
-            armPower = 0;
-            telemetry.addData("Deb", "Upper Limit");
-        }
-
-        viperArm.setPower(armPower);
-        telemetry.addData("arm power", "%4.2f", armPower);
-        telemetry.addData("arm position (Ticks)", "%4.2f", viperArm.getCurrentPosition());
+//        double max = 1;
+//        max = Math.max(max, Math.abs(armPower));
+//        if (armPower > max) {
+//            armPower = max;
+//        }
+//
+//        if (touchSensor.isPressed() && armPower < 0) {
+//            armPower = 0;
+//            viperArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            telemetry.addData("Deb", "Pressed");
+//        } else {
+//            telemetry.addData("Deb", "Not Pressed");
+//        }
+//        //upper limit
+//        if (viperArm.getCurrentPosition() >= 11300 && armPower > 0){ //this is viper max height
+//            armPower = 0;
+//            telemetry.addData("Deb", "Upper Limit");
+//        }
+//
+//        viperArm.setPower(armPower);
+//        telemetry.addData("arm power", "%4.2f", armPower);
+        telemetry.addData("arm position (Ticks)", viperArm.getCurrentPosition());
     }
 }
